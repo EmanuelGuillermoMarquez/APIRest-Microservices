fed2d3f87a1f3c6edfb40973d5435796053ca10e
Emanuel <marquezema127@gmail.com>
2023-09-15 20:57:07 -0300

    utils and middlewares added

--- API_Gateway/src/server.js
+++ API_Gateway/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
-
 const {createProxyMiddleware} = require("http-proxy-middleware");
+
 const proxyMiddleware = createProxyMiddleware;
 
 const server = express();
