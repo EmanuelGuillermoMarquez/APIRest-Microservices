7b3cf3386adf293517c6383739867b78ebdc5022
Emanuel <marquezema127@gmail.com>
2023-09-23 19:14:48 -0300

    routes features added

--- Planets/src/server.js
+++ Planets/src/server.js
@@ -8,7 +8,7 @@ server.use(morgan("dev"));
 
 server.use(require("./routes"));
 server.use("*", (req, res) => res.status(404).send("Not found"));
-ver.use((err, req, res, next) => res.status(err.statusCode || err.response?.status || 500).send({
+server.use((err, req, res, next) => res.status(err.statusCode || err.response?.status || 500).send({
     error: true,
     message: err.response?.data?.message || err.message
 }));
