4bc9591037d8496a7a36036cd3b1abdc7204f9bb
Emanuel <marquezema127@gmail.com>
2023-09-30 09:05:37 -0300

    finish

--- API_Gateway/src/routes/index.js
+++ API_Gateway/src/routes/index.js
@@ -2,6 +2,9 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get("/", (req, res) => res.status(200).send("Hello World. Welcome to my microservices API Rest"));
+router.get("/", (req, res) => res.status(200).send({
+    error: false,
+    message: "Hello World. Welcome to my microservices API Rest"
+}));
 
 module.exports = router;
